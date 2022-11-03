7f5b075ddfef6011d22869e1561bc3f7744184da
Fabiano Abbey <abbeyfabiano@gmail.com>
2022-11-04 01:13:01 +0700

    menambahkan file list kata dan gambar hangman

---
+++ word_list.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from string import ascii_uppercase
+
+word_list = ["INDONESIA","MALAYSIA","THAILAND","LAOS","NORWAY","ARGENTINA","ICELAND","GERMANY","TURKEY","JAPAN"]
+
+photos = [PhotoImage(file="images/hang0.png"), PhotoImage(file="images/hang1.png"), PhotoImage(file="images/hang2.png"), PhotoImage(file="images/hang3.png"),
+          PhotoImage(file="images/hang4.png"), PhotoImage(file="images/hang5.png"), PhotoImage(file="images/hang6.png"), PhotoImage(file="images/hang7.png"),
+          PhotoImage(file="images/hang8.png"), PhotoImage(file="images/hang9.png"), PhotoImage(file="images/hang10.png"), PhotoImage(file="images/hang11.png")]
