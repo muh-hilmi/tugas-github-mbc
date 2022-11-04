b9f092825e04ea6c859d849fab2ca4eb740eece1
Rafi Andhika <andhika.rafi.2003@gmail.com>
2022-11-04 07:01:36 +0700

    update file Hangman.py, menggabungkan Hangman.py dengan Word_list.py

--- Hangman.py
+++ Hangman.py
@@ -1,13 +1,28 @@
 from tkinter import *
 from string import ascii_uppercase
 
-
 window = Tk()
 window.title("Hangman")
 
+word_list = ["INDONESIA","MALAYSIA","THAILAND","LAOS","NORWAY","ARGENTINA","ICELAND","GERMANY","TURKEY","JAPAN"]
+
+photos = [PhotoImage(file="images/hang0.png"), PhotoImage(file="images/hang1.png"), PhotoImage(file="images/hang2.png"), PhotoImage(file="images/hang3.png"),
+          PhotoImage(file="images/hang4.png"), PhotoImage(file="images/hang5.png"), PhotoImage(file="images/hang6.png"), PhotoImage(file="images/hang7.png"),
+          PhotoImage(file="images/hang8.png"), PhotoImage(file="images/hang9.png"), PhotoImage(file="images/hang10.png"), PhotoImage(file="images/hang11.png")]
+
+def guess():
+    global numberOfGuesses
+
+imglabel = Label(window)
+imglabel.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+imglabel.config(image=photos[0])
+
+lblword=StringVar()
+Label(window, textvariable=lblword, font=("consolas 24")).grid(row=0, column=3, columnspan=5, padx=10)
+
 n=0
 for c in ascii_uppercase:
-    Button(window, text=c, font=("Arial", 18), width=4).grid(row=1+n//9, column=n%9)
+    Button(window, text=c, command=lambda c=c : guess(c), font=("Arial", 18), width=4).grid(row=1+n//9, column=n%9)
     n+=1
 
 window.mainloop()
