7c18ea4d6c571d8451b3f76c3d84e50f0fce2a43
muhhilmi <mailhilmi@gmail.com>
2022-11-04 11:51:35 +0700

    update algorithm guess() and delete word_list.py

--- Hangman.py
+++ Hangman.py
@@ -1,12 +1,13 @@
 from tkinter import *
+from tkinter import messagebox
 from string import ascii_uppercase
 import random
-
 
 window = Tk()
 window.title("Hangman")
 
-word_list = ["INDONESIA","MALAYSIA","THAILAND","LAOS","NORWAY","ARGENTINA","ICELAND","GERMANY","TURKEY","JAPAN"]
+word_list = ["INDONESIA","MALAYSIA","THAILAND","LAOS","NORWAY","ARGENTINA","ICELAND","GERMANY","TURKEY","JAPAN", 
+             "INDIA","MOROCCO","PAKISTAN","NEPAL","HAITI","MEXICO"]
 
 photos = [PhotoImage(file="images/hang0.png"), PhotoImage(file="images/hang1.png"), PhotoImage(file="images/hang2.png"), PhotoImage(file="images/hang3.png"),
           PhotoImage(file="images/hang4.png"), PhotoImage(file="images/hang5.png"), PhotoImage(file="images/hang6.png"), PhotoImage(file="images/hang7.png"),
@@ -20,14 +21,26 @@ def newGame():
     the_word = random.choice(word_list)
     the_word_withSpaces = " ".join(the_word)
     lblword.set(" ".join("_"*len(the_word)))
-    
 
-
-def guess():
+def guess(letter):
     global numberOfGuesses
-    txt = list(the_word_withSpaces)
-    guessed = list(lblword.get())
-
+    if numberOfGuesses < 11 :
+        txt = list(the_word_withSpaces)
+        guessed = list(lblword.get())
+        if the_word_withSpaces.count(letter)>0:
+            for c in range(len(txt)):
+                if txt[c]==letter:
+                    guessed[c]=letter
+                lblword.set("".join(guessed))
+                if lblword.get()==the_word_withSpaces:
+                    messagebox.showinfo("Hangman!", "Yeay!, Kamu berhasil nebak")
+                    newGame()
+        else:
+            numberOfGuesses+=1
+            imglabel.config(image=photos[numberOfGuesses])
+            if numberOfGuesses == 11:
+                messagebox.showwarning("Hangman!", "Yah kalah, silahkan coba lagi")
+                newGame()
 
 imglabel = Label(window)
 imglabel.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
@@ -41,4 +54,8 @@ for c in ascii_uppercase:
     Button(window, text=c, command=lambda c=c : guess(c), font=("Arial", 18), width=4).grid(row=1+n//9, column=n%9)
     n+=1
 
+Button(window, text = "New Game", command=lambda:newGame(), font=("Arial", 10)).grid(row=3, column=8, sticky="NSWE")
+
+
+newGame()
 window.mainloop()
